dc81b13435a7d8bd5be4e99fb5a804b6afd78017
Viktoriia Babiuk <cloud14777@gmail.com>
2021-07-04 20:59:50 +0300

    deleted unnecessary code

--- homework-3/script.js
+++ homework-3/script.js
@@ -4,8 +4,6 @@
 function getMaxDigit(number) {
     return Math.max(...number.toString().split('').map(Number))
 }
-// console.log(getMaxDigit(105876));
-
 
 // ------Function №2------
 // Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл
@@ -22,16 +20,12 @@ function numberToThePow(x, y) {
     return result;
 }
 
-// console.log(numberToThePow(2, -2))
-
 // ------Function №3------
 // Створити функцію, яка форматує ім'я, роблячи першу букву великою. ("влад" -> "Влад", "вЛАД" -> "Влад" і так далі);
 
 function fixedName(name) {
     return name[0].toUpperCase() + name.slice(1).toLowerCase();
 }
-// console.log(fixedName('vIkTORiA'))
-
 
 // ------Function №4------
 // Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати. (Податок = 18% + 1.5% -> 19.5%). Приклад: 1000 -> 805
@@ -39,7 +33,6 @@ function fixedName(name) {
 function netSalary(salary, tax) {
     return salary - (salary * (tax / 100));
 }
-// console.log(netSalary(1000, 19.5))
 
 
 // ------Function №5------
@@ -48,8 +41,6 @@ function netSalary(salary, tax) {
 function getRandomNumber(n, m) {
     return Math.ceil(Math.random() * (m - n) + n);
 }
-//   console.log(getRandomNumber(-100, 1000))
-
 
 // ------Function №6------
 // Створити функцію, яка рахує скільки разів певна буква повторюється в слові. Приклад: countLetter("а", "Асталавіста") -> 4
@@ -65,8 +56,6 @@ function countLetter(letter, word) {
     }
     return count;
 }
-// console.log(countLetter('O', 'CursOre one LOVE' ))
-
 
 // ------Function №8------
 // Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.
@@ -79,8 +68,6 @@ function getRandomPassword(Password = 8) {
     }
     return lengthPassword
 }
-// console.log(getRandomPassword(2))
-
 
 // ------Function №10------
 // Створіть функцію, яка перевіряє, чи є слово паліндромом. Приклад: isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу гусеня") -> true
@@ -92,9 +79,6 @@ function isPalyndrom(expression) {
         return true;
     } else return false;
 }
-// console.log(isPalyndrom("Я, н,е ,су гусеня"))
-
-
 
 document.writeln(`
 <li>Function №1: ${getMaxDigit(123456)}</li>
